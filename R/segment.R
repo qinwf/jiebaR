077ac46a0da22f0e5a46a85f5acb09bb082aff56
qinwf <mail@qinwenfeng.com>
2014-10-26 15:11:32 +0800

    change symbol

--- R/segment.R
+++ R/segment.R
@@ -8,13 +8,17 @@ mixcut<-function(code,file=NULL,output=paste0("rjieba",Sys.time(),".dat")
                  ,dict=NULL,hmm=NULL,user=NULL,symbol=F){
   if (!is.character(code) || length(code) != 1 )
     stop("Argument 'code' must be an string.")
+  if(symbol==F){
+    code<-gsub("[^\u4e00-\u9fa5a-zA-Z0-9]", " ", code)
+  }
+  code <- gsub("^\\s+|\\s+$", "", gsub("\\s+", " ", code))
   result<-mixcutc(code)
   if(symbol==F){
-    result<-grep("[^[:punct:][:space:]，。、《》：；“”‘’【】｛｝？！（）]", result,value = T)
+    result<-grep("[^[:space:]]", result,value = T)
   }
   if(.Platform$OS.type=="windows"){
     result<-iconv(result,"UTF-8","GBK")
-    return (result[!is.na(result)])
+    return (result)
   }
   return(result)
 }
