42e5239930193d4b0a043707c53a3538ac364c7e
qinwf <mail@qinwenfeng.com>
2014-10-26 14:21:53 +0800

    regex

--- R/segment.R
+++ R/segment.R
@@ -1,9 +1,12 @@
 #' @export
 mixcut<-function(code,file=NULL,output=paste0("rjieba",Sys.time(),".dat")
-                 ,dict=NULL,hmm=NULL,user=NULL){
+                 ,dict=NULL,hmm=NULL,user=NULL,symbol=F){
   if (!is.character(code) || length(code) != 1 )
     stop("Argument 'code' must be an string.")
   result<-mixcutc(code)
+  if(symbol==F){
+    result<-grep("[^[:punct:][:space:]，。、]", result,value = T)
+  }
   if(.Platform$OS.type=="windows"){
     return (iconv(result,"UTF-8","GBK"))
   }
