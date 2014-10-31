2fb98bd84d58c87d48d37ddcfc30d0ff3b7c27f0
qinwf <mail@qinwenfeng.com>
2014-11-01 07:48:01 +0800

    Fix loading error

--- R/load.R
+++ R/load.R
@@ -1,11 +1,11 @@
 
 ##Path
 
-DICTPATH<-file.path(find.package("jiebaR"),"dict","jieba.dict.utf8")
-HMMPATH <-file.path(find.package("jiebaR"),"dict","hmm_model.utf8")
-USERPATH<-file.path(find.package("jiebaR"),"dict","user.dict.utf8")
-STOPPATH<-file.path(find.package("jiebaR"),"dict","stop_words.utf8")
-IDFPATH <-file.path(find.package("jiebaR"),"dict","idf.utf8")
+DICTPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","jieba.dict.utf8")
+HMMPATH <-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","hmm_model.utf8")
+USERPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","user.dict.utf8")
+STOPPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","stop_words.utf8")
+IDFPATH <-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","idf.utf8")
 
 ##loadModule
 
--- R/zzz.R
+++ R/zzz.R
@@ -9,5 +9,22 @@ NULL
     if (.Platform$OS.type == "windows") {
       Sys.setlocale( locale = "English")
     }
-
+#     ##Path
+#     
+#     DICTPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","jieba.dict.utf8")
+#     HMMPATH <-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","hmm_model.utf8")
+#     USERPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","user.dict.utf8")
+#     STOPPATH<-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","stop_words.utf8")
+#     IDFPATH <-file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","idf.utf8")
+#     
 }
+
+setLoadAction(
+  function(ns){ 
+    assign("DICTPATH", file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","jieba.dict.utf8"), envir = ns)
+    assign("HMMPATH", file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","hmm_model.utf8"), envir = ns)
+    assign("USERPATH", file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","user.dict.utf8"), envir = ns)
+    assign("STOPPATH", file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","stop_words.utf8"), envir = ns)
+    assign("IDFPATH", file.path(find.package("jiebaR",lib.loc=.libPaths()),"dict","idf.utf8"), envir = ns)
+    
+    })
