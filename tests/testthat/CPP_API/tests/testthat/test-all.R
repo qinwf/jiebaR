744f7adc94b0cd1b7bdeb6c8dbb5f86291d395c7
qinwf <mail@qinwenfeng.com>
2015-08-28 08:40:14 +0800

    more test on API

--- tests/testthat/CPP_API/tests/testthat/test-all.R
+++ tests/testthat/CPP_API/tests/testthat/test-all.R
@@ -1,6 +1,22 @@
 context("CPP API")
 
 test_that("hmm cpp api",{
-  cutter = jiebaRapi:::hmm_ptr(jiebaR:::HMMPATH)
+  cutter = jiebaRapi:::hmm_ptr(jiebaR::HMMPATH)
   expect_equal(jiebaRapi:::hmm_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
 })
+
+test_that("mix cpp api",{
+  cutter = jiebaRapi:::mix_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH)
+  expect_equal(jiebaRapi:::mix_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
+})
+
+test_that("mp cpp api",{
+  cutter = jiebaRapi:::mp_ptr(jiebaR::DICTPATH,jiebaR::USERPATH)
+  expect_equal(jiebaRapi:::mp_cut("this is test",cutter),c("t", "h", "i", "s", " ", "i", "s", " ", "t", "e", "s", "t"))
+})
+
+test_that("query cpp api",{
+  cutter = jiebaRapi:::query_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,20)
+  expect_equal(jiebaRapi:::query_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
+})
+
--- tests/testthat/C_API/tests/testthat/test-all.R
+++ tests/testthat/C_API/tests/testthat/test-all.R
@@ -1,6 +1,22 @@
 context("C API")
 
 test_that("hmm c api",{
-  cutter = jiebaRapi:::hmm_ptr(jiebaR:::HMMPATH)
+  cutter = jiebaRapi:::hmm_ptr(jiebaR::HMMPATH)
   expect_equal(jiebaRapi:::hmm_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
 })
+
+test_that("mix c api",{
+  cutter = jiebaRapi:::mix_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH)
+  expect_equal(jiebaRapi:::mix_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
+})
+
+test_that("mp c api",{
+  cutter = jiebaRapi:::mp_ptr(jiebaR::DICTPATH,jiebaR::USERPATH)
+  expect_equal(jiebaRapi:::mp_cut("this is test",cutter),c("t", "h", "i", "s", " ", "i", "s", " ", "t", "e", "s", "t"))
+})
+
+test_that("query c api",{
+  cutter = jiebaRapi:::query_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,20)
+  expect_equal(jiebaRapi:::query_cut("this is test",cutter),c("this", " ", "is", " ", "test"))
+})
+
