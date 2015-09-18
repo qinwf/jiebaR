089fadaa4c8b2f83459d2653684204adebea9889
qinwf <mail@qinwenfeng.com>
2015-09-18 22:22:30 +0800

    fix filter_segment

--- R/filter.R
+++ R/filter.R
@@ -5,6 +5,6 @@
 #' @param filter_words a string vector of words to be removed.
 #' @export
 filter_segment<- function(input,filter_words){
-  grep(sprintf("(*UCP)(%s)", paste(filter_words, collapse = "|")), input,
+  grep(sprintf("(*UCP)^(%s)$", paste(filter_words, collapse = "|")), input,
        perl = TRUE,invert = TRUE,value = TRUE)
 }
