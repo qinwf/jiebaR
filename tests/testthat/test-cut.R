4fe806b7ab041d49ac7aca8ce5a5cfee897b61c0
qinwf <mail@qinwenfeng.com>
2015-09-20 00:48:49 +0800

    tests for 2010-10-10 10.2

--- tests/testthat/test-cut.R
+++ tests/testthat/test-cut.R
@@ -6,6 +6,12 @@ test_that("segmentation", {
   expect_identical({cutter <= "This is Test"}, c("This"," ","is"," ","Test"))
   expect_identical({cutter <= "AK47"}, c("AK47"))
   expect_identical({cutter <= "no Chinese for CRAN 2015-07-25"},c("no"," ", "Chinese", " ","for", " ","CRAN"," ", "2015-07-25"))
+  cutter$symbol =F
+  expect_identical({cutter <= "2015-07-25"},c("2015-07-25"))
+  expect_identical({cutter <= "10.2"},c("10.2"))
+  expect_identical({cutter <= "10.2。"},c("10.2"))
+  cutter$symbol=T
+  expect_identical({cutter <= "10.2。"},c("10.2","。"))
 })
 
 cutter$symbol = FALSE
