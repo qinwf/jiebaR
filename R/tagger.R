bedad983aec12452bd5ba1a2a7728158a68df9fe
qinwf <mail@qinwenfeng.com>
2015-09-20 22:55:39 +0800

    tagger 2012-12-12 10.2

--- R/tagger.R
+++ R/tagger.R
@@ -174,10 +174,10 @@ tagl <- function(code, jiebar, symbol, lines, output, encoding, write_file,FILES
 
 tagw <- function(code, jiebar,  symbol, FILESMODE) {
   
-  if (symbol == F) {
-    code <- gsub("[^\u2e80-\u3000\u3021-\ufe4fa-zA-Z0-9]", " ", code)
-  } 
-  #  code <- gsub("^\\s+|\\s+$", "", gsub("\\s+", " ", code))
+#   if (symbol == F) {
+#     code <- gsub("[^\u2e80-\u3000\u3021-\ufe4fa-zA-Z0-9]", " ", code)
+#   } 
+#   #  code <- gsub("^\\s+|\\s+$", "", gsub("\\s+", " ", code))
   if(jiebar$bylines == FALSE){
     if(length(code) > 1){
       code <- paste(code, collapse = " ")
@@ -193,6 +193,9 @@ tagw <- function(code, jiebar,  symbol, FILESMODE) {
     
     if (.Platform$OS.type == "windows") {
       Encoding(result)<-"UTF-8"
+    }
+    if (symbol == F) {
+      result <- grep("(*UCP)^[^\u2e80-\u3000\u3021-\ufe4fa-zA-Z0-9]*$", result, perl = TRUE,value = TRUE,invert = T)
     }
   } else{
     
@@ -211,6 +214,9 @@ tagw <- function(code, jiebar,  symbol, FILESMODE) {
       
       if (.Platform$OS.type == "windows") {
         Encoding(tmp_result)<-"UTF-8"
+      }
+      if (symbol == F) {
+        tmp_result <- grep("(*UCP)^[^\u2e80-\u3000\u3021-\ufe4fa-zA-Z0-9]*$", tmp_result, perl = TRUE,value = TRUE,invert = T)
       }
       result[[num]] = tmp_result
     }
