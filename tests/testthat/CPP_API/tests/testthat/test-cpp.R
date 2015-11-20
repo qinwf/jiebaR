962d86bca0b1adc8a0ca3f619f065f2e2f8937b9
qinwf <mail@qinwenfeng.com>
2015-11-20 11:25:43 +0800

    more tests for apis

--- tests/testthat/CPP_API/tests/testthat/test-cpp.R
+++ tests/testthat/CPP_API/tests/testthat/test-cpp.R
@@ -29,3 +29,40 @@ test_that("query cpp api",{
   expect_equal(jiebaRapi:::query_cut("this is test",cutter),c("test"))
 })
 
+test_that("keys cpp api",{
+  cutter = jiebaRapi:::key_ptr(4,jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::IDFPATH,jiebaR::STOPPATH)
+
+  expect_equal(jiebaRapi:::key_cut("this is test",cutter),c("test"))
+
+  expect_equal(jiebaRapi:::key_keys("this is test",cutter),structure("this is test", .Names = "11.7392"))
+
+  expect_equal(jiebaRapi:::key_tag("this is test",cutter),structure("test", .Names = "11.7392"))
+  
+})
+
+test_that("simhahs cpp api",{
+  cutter = jiebaRapi:::sim_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,
+                               jiebaR::IDFPATH,jiebaR::STOPPATH)
+  
+  expect_equal(jiebaRapi:::sim_sim("this is test",3,cutter),
+               structure(list(simhash = "13694348408744233390",
+                              keyword = structure("test", .Names = "11.7392")),
+                         .Names = c("simhash", "keyword")))
+  
+  expect_equal(jiebaRapi:::sim_distance("this is test","this is test",3,cutter),
+               structure(list(distance = "0", lhs = 
+                                structure("test", .Names = "11.7392"), 
+               rhs = structure("test", .Names = "11.7392")), 
+               .Names = c("distance", 
+                          "lhs", "rhs")))
+})
+
+test_that("tag cpp api",{
+  cutter = jiebaRapi:::tag_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH,NULL)
+  expect_equal(jiebaRapi:::tag_tag("this is test",cutter),
+               structure(c("this", " ", "is", " ", "test"), 
+                         .Names = c("eng", "x", "eng", "x", "eng"))
+               )
+  cutter = jiebaRapi:::tag_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH,jiebaR::STOPPATH)
+  expect_equal(jiebaRapi:::tag_tag("this is test",cutter),structure("test", .Names = "eng"))
+})
--- tests/testthat/C_API/tests/testthat/test-c.R
+++ tests/testthat/C_API/tests/testthat/test-c.R
@@ -30,3 +30,41 @@ test_that("query c api",{
   expect_equal(jiebaRapi:::query_cut("this is test",cutter),c("test"))
 })
 
+
+test_that("keys c api",{
+  cutter = jiebaRapi:::key_ptr(4,jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::IDFPATH,jiebaR::STOPPATH)
+  
+  expect_equal(jiebaRapi:::key_cut("this is test",cutter),c("test"))
+  
+  expect_equal(jiebaRapi:::key_keys("this is test",cutter),structure("this is test", .Names = "11.7392"))
+  
+  expect_equal(jiebaRapi:::key_tag("this is test",cutter),structure("test", .Names = "11.7392"))
+  
+})
+
+test_that("simhahs c api",{
+  cutter = jiebaRapi:::sim_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,
+                               jiebaR::IDFPATH,jiebaR::STOPPATH)
+  
+  expect_equal(jiebaRapi:::sim_sim("this is test",3,cutter),
+               structure(list(simhash = "13694348408744233390",
+                              keyword = structure("test", .Names = "11.7392")),
+                         .Names = c("simhash", "keyword")))
+  
+  expect_equal(jiebaRapi:::sim_distance("this is test","this is test",3,cutter),
+               structure(list(distance = "0", lhs = 
+                                structure("test", .Names = "11.7392"), 
+                              rhs = structure("test", .Names = "11.7392")), 
+                         .Names = c("distance", 
+                                    "lhs", "rhs")))
+})
+
+test_that("tag c api",{
+  cutter = jiebaRapi:::tag_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH,NULL)
+  expect_equal(jiebaRapi:::tag_tag("this is test",cutter),
+               structure(c("this", " ", "is", " ", "test"), 
+                         .Names = c("eng", "x", "eng", "x", "eng"))
+  )
+  cutter = jiebaRapi:::tag_ptr(jiebaR::DICTPATH,jiebaR::HMMPATH,jiebaR::USERPATH,jiebaR::STOPPATH)
+  expect_equal(jiebaRapi:::tag_tag("this is test",cutter),structure("test", .Names = "eng"))
+})
