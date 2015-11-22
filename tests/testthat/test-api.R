ff0ba715036ef5fe3a79478077e0302b2533401a
qinwf <mail@qinwenfeng.com>
2015-11-22 10:37:03 +0800

    fix for xcode7.1

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -3,13 +3,13 @@ library(devtools)
 library(testthat)
 
 test_that("C_API",{
-  install_local("./C_API")
+  install("./C_API")
   expect_true(!any(as.data.frame(test("./C_API"))[["error"]]) )
   # expect_error(not(check(as.package("./C_API"))))
 })
 
 test_that("CPP_API",{
-  install_local("./CPP_API")
+  install("./CPP_API")
   expect_true(!any(as.data.frame(test("./CPP_API"))[["error"]]) )
   # expect_error(not(check(as.package("./CPP_API"))))
 })
