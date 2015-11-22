2fac445770017662377907aa476532ce4902b6bd
qinwf <mail@qinwenfeng.com>
2015-11-22 10:03:17 +0800

    replace expect_error() with test()

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -3,11 +3,13 @@ library(devtools)
 library(testthat)
 
 test_that("C_API",{
-
-  expect_error(not(check(as.package("./C_API"))))
+  install_local("./C_API")
+  expect_true(!any(as.data.frame(test("./C_API"))[["error"]]) )
+  # expect_error(not(check(as.package("./C_API"))))
 })
 
 test_that("CPP_API",{
-
-  expect_error(not(check(as.package("./CPP_API"))))
+  install_local("./CPP_API")
+  expect_true(!any(as.data.frame(test("./CPP_API"))[["error"]]) )
+  # expect_error(not(check(as.package("./CPP_API"))))
 })
