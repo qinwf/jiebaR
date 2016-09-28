594b96a0f9724964661d74cfeb3d2c84c4f8193e
qinwf <mail@qinwenfeng.com>
2016-09-28 12:03:07 +0800

    Add: default tag for new_user_word

--- R/worker_func.R
+++ R/worker_func.R
@@ -16,9 +16,13 @@ query_threshold = function(worker, number){
 #' 
 #' @param worker a jieba worker
 #' @param words the new words
-#' @param tags the new words tags
+#' @param tags the new words tags, default "n"
+#' @examples
+#' cc = worker()
+#' new_user_word(cc, "test")
+#' new_user_word(cc, "do", "v")
 #' @export
-new_user_word = function(worker, words, tags){
+new_user_word = function(worker, words, tags = rep("n", length(words))){
   stopifnot(inherits(worker,"jieba"), length(words) == length(tags), is.character(words), is.character(tags))
   if (.Platform$OS.type == "windows") {
     words <- enc2utf8(words)
